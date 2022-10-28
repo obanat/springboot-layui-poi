68a532318a5deaecba6927c4d1ef7c772a5558a6
obanat <obana@163.com>
2022-10-28 21:26:47 +0800

    Create seInfo.java

---
+++ src/main/java/com/mudemo/model/seInfo.java
@@ -0,0 +1,20 @@
+package com.mudemo.model;
+
+import lombok.Data;
+
+/**
+ * @author obana
+ * @version 1.0
+ * @date 2022/10/24 19:16
+ * @description
+ * @modify
+ */
+@Data
+public class seInfo {
+    private int id;
+    private String name;
+    private String plName;
+    private String dept;
+    private String description;
+    private String welinkId;
+}
