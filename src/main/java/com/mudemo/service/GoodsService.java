1a10741a3dea0288a42b811ecbf6528613e0f4ff
obanat <obana@163.com>
2022-10-28 21:28:09 +0800

    Update GoodsService.java

--- src/main/java/com/mudemo/service/GoodsService.java
+++ src/main/java/com/mudemo/service/GoodsService.java
@@ -1,18 +1,31 @@
 package com.mudemo.service;
 
 import com.alibaba.fastjson.JSONObject;
+import com.mudemo.model.seInfo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 public interface GoodsService {
-    JSONObject getGoodsList(int pageNum, int pageSize);
+    JSONObject getTasksList(int pageNum, int pageSize);
 
-    JSONObject addGood(JSONObject request);
+    JSONObject addTask(JSONObject request);
 
-    JSONObject updateGood(JSONObject request);
+    JSONObject updateTask(JSONObject request);
 
-    JSONObject deleteGood(int id);
+    JSONObject deleteTask(int id);
 
-    JSONObject searchGoods(JSONObject request);
+    JSONObject searchTasks(JSONObject request);
+
+    JSONObject getSeList(int pageNum, int pageSize);
+
+    JSONObject addSe(JSONObject request);
+
+    List<seInfo> getSeList2();
+
+    void putReportStr(int taskId, String sheetName, int nameCol, int valueCol, String reportStr);
+
+    JSONObject getTaskReportInfo(int taskId);
 }
